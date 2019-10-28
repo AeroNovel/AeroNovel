44269be9ebfa833022cb8b81efdc05da0bbeab78
Aeroblast <>
2019-10-28 12:36:56 +0800

    Update Epub2Comment.cs

--- AeroNovelTool/src/Epub2Comment.cs
+++ AeroNovelTool/src/Epub2Comment.cs
@@ -5,6 +5,11 @@ public class Epub2Comment
     public static void Proc(string path)
     {
         Directory.CreateDirectory("epub2comment_output");
+        if (!File.Exists(path))
+        {
+            Log.log("[Error]File not exits!");
+            return;
+        }
         Epub e = new Epub(path);
         e.items.ForEach(
             (i) =>
@@ -16,31 +21,54 @@ public class Epub2Comment
     }
     static void ProcXHTML(TextItem i)
     {
-        Log.log("[Info]"+i.fullName);
-        string name=Path.GetFileNameWithoutExtension(i.fullName);
-        string r=i.data.Replace("\r","").Replace("\n","");
-        Match m=Regex.Match(r,"<body(.*)</body>");
-        if(!m.Success){Log.log("[Error]body?");return;}
-        r=m.Groups[0].Value;
-        XFragment f=new XFragment(r,0);
-        string txt="";
-        string counter="";
-        f.parts.ForEach((p)=>
+        Log.log("[Info]" + i.fullName);
+        string name = Path.GetFileNameWithoutExtension(i.fullName);
+        string r = i.data.Replace("\r", "").Replace("\n", "");
+        Match m = Regex.Match(r, "<body(.*)</body>");
+        if (!m.Success) { Log.log("[Error]body?"); return; }
+        r = m.Groups[0].Value;
+        XFragment f = new XFragment(r, 0);
+        string txt = "";
+        string counter = "";
+        string temp = "";
+        foreach (var p in f.parts)
         {
-            if(p.GetType()==typeof(XText)){txt+=Util.Trim(p.originalText);counter+=Util.Trim(p.originalText);}
-            if(p.GetType()==typeof(XTag))
+            if (p.GetType() == typeof(XText))
             {
-                XTag p0=(XTag)p;
-                if(p.type==PartType.tag_start&&p0.tagname=="rt"){txt+="(";}
-                if(p.type==PartType.tag_end&&p0.tagname=="rt"){txt+=")";}
-                 if(p.type==PartType.tag_start&&p0.tagname=="p"){txt+="##";}
-                if(p.type==PartType.tag_end&&p0.tagname=="p"){txt+="\r\n\r\n##——————\r\n";}
-                if(p.type==PartType.tag_end&&p0.tagname=="div"){txt+="\r\n\r\n##——————\r\n";}
+                string trimed = Util.Trim(p.originalText);
+                txt += trimed;
+                counter += trimed;
+                if (trimed.Length > 0)
+                    switch (trimed[0])
+                    {
+                        case '「':
+                            temp = "「」";
+                            break;
+                        case '『':
+                            temp = "『』";
+                            break;
+                        default:
+                            temp = "";
+                            break;
+                    }
+                else
+                {
+                    temp = "";
+                }
             }
-        });
-        if(Util.Trim(counter).Length>0)
-        File.WriteAllText("epub2comment_output/"+name+".txt",txt);
-        
+            if (p.GetType() == typeof(XTag))
+            {
+                XTag p0 = (XTag)p;
+                if (p.type == PartType.tag_start && p0.tagname == "rt") { txt += "("; }
+                if (p.type == PartType.tag_end && p0.tagname == "rt") { txt += ")"; }
+                if (p.type == PartType.tag_start && p0.tagname == "p") { txt += "##"; }
+                if (p.type == PartType.tag_end && p0.tagname == "p") { txt += "\r\n" + temp + "\r\n##——————\r\n"; }
+                if (p.type == PartType.tag_end && p0.tagname == "div") { txt += "\r\n\r\n##——————\r\n"; }
+            }
+        }
+        if (Util.Trim(counter).Length > 0)
+            File.WriteAllText("epub2comment_output/" + name + ".txt", txt);
+
 
     }
 
